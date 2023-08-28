82081e5399379fb3c632a1fdabff44855103e1e4
GrigoriyMo <grigoriymo@MacBook-Pro--Mo.local>
2023-08-28 21:08:44 +0300

    test change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function returnColor(){
     return "#0bfc03";
 }
 
-console.log('started', new Date())
+console.log('started!', new Date())
 console.log(returnColor())
 
 module.exports = returnColor;
